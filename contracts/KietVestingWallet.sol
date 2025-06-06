6178760f66f0a17f02a03961a3c4eab22df3b001
duong-kiet <bintotet20041012@gmail.com>
2025-06-06 17:24:04 +0700

    feat: minimal proxy + one wallet one role

---
+++ contracts/KietVestingWallet.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.3.0) (finance/VestingWallet.sol)
+pragma solidity ^0.8.20;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract KietVestingWallet is Context {
+    event EtherReleased(uint256 amount);
+    event ERC20Released(address indexed token, uint256 amount);
+
+    uint256 private _released;
+    mapping(address token => uint256) private _erc20Released;
+    uint64 private _start;
+    uint64 private _duration;
+    address private _beneficiary;
+
+    bool private _initialized;
+
+    /**
+     * @dev Sets the beneficiary (owner), the start timestamp and the vesting duration (in seconds) of the vesting
+     * wallet.
+     */
+
+    function initialized(address beneficiary, uint64 startTimestamp, uint64 durationSeconds) external {
+        require(!_initialized, "Already initialized");
+        _initialized = true;
+
+        _start = startTimestamp;
+        _duration = durationSeconds;
+
+        // require(beneficiary != address(0), "Address is not exist");
+        _beneficiary = beneficiary; 
+    }
+
+    /**
+     * @dev The contract should be able to receive Eth.
+     */
+    receive() external payable virtual {}
+
+    /**
+     * @dev Getter for the start timestamp.
+     */
+    function start() public view virtual returns (uint256) {
+        return _start;
+    }
+
+    /**
+     * @dev Getter for the vesting duration.
+     */
+    function duration() public view virtual returns (uint256) {
+        return _duration;
+    }
+
+    /**
+     * @dev Getter for the end timestamp.
+     */
+    function end() public view virtual returns (uint256) {
+        return start() + duration();
+    }
+
+    /**
+     * @dev Amount of eth already released
+     */
+    function released() public view virtual returns (uint256) {
+        return _released;
+    }
+
+    /**
+     * @dev Amount of token already released
+     */
+    function released(address token) public view virtual returns (uint256) {
+        return _erc20Released[token];
+    }
+
+    /**
+     * @dev Getter for the amount of releasable eth.
+     */
+    function releasable() public view virtual returns (uint256) {
+        return vestedAmount(uint64(block.timestamp)) - released();
+    }
+
+    /**
+     * @dev Getter for the amount of releasable `token` tokens. `token` should be the address of an
+     * {IERC20} contract.
+     */
+    function releasable(address token) public view virtual returns (uint256) {
+        return vestedAmount(token, uint64(block.timestamp)) - released(token);
+    }
+
+    /**
+     * @dev Release the native token (ether) that have already vested.
+     *
+     * Emits a {EtherReleased} event.
+     */
+    function release() public virtual {
+        uint256 amount = releasable();
+        _released += amount;
+        emit EtherReleased(amount);
+        Address.sendValue(payable(_beneficiary), amount);
+    }
+
+    /**
+     * @dev Release the tokens that have already vested.
+     *
+     * Emits a {ERC20Released} event.
+     */
+    function release(address token) public virtual {
+        uint256 amount = releasable(token);
+        _erc20Released[token] += amount;
+        emit ERC20Released(token, amount);
+        SafeERC20.safeTransfer(IERC20(token), _beneficiary, amount);
+    }
+
+    /**
+     * @dev Calculates the amount of ether that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(uint64 timestamp) public view virtual returns (uint256) {
+        return _vestingSchedule(address(this).balance + released(), timestamp);
+    }
+
+    /**
+     * @dev Calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(address token, uint64 timestamp) public view virtual returns (uint256) {
+        return _vestingSchedule(IERC20(token).balanceOf(address(this)) + released(token), timestamp);
+    }
+
+    /**
+     * @dev Virtual implementation of the vesting formula. This returns the amount vested, as a function of time, for
+     * an asset given its total historical allocation.
+     */
+    function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view virtual returns (uint256) {
+        if (timestamp < start()) {
+            return 0;
+        } else if (timestamp >= end()) {
+            return totalAllocation;
+        } else {
+            return (totalAllocation * (timestamp - start())) / duration();
+        }
+    }
+}
--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/finance/VestingWallet.sol";
+// import "@openzeppelin/contracts/finance/VestingWallet.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MinimalProxy.sol";
+import "./KietVestingWallet.sol";
 
-contract Vesting is Ownable {
+contract Vesting is MinimalProxy, Ownable {
    IERC20 public token;
+   address public kietVestingWalletAddress;
 
    enum Role {
       Founder,
@@ -17,71 +20,78 @@ contract Vesting is Ownable {
    uint constant SECONDS_IN_DAY = 86400;
    uint constant DAYS_IN_YEAR = 365;
 
-   // uint cliffFounder = SECONDS_IN_DAY * 30 * 12; // 1 year
-   // uint cliffAdvisor =  SECONDS_IN_DAY * 30 * 6; // 6 months 
-   // uint cliffEmployee =  SECONDS_IN_DAY * 30 * 6; // 6 months
-   // uint vestingTimeFounder = SECONDS_IN_DAY * 30 * 12 * 4; // 4 years
-   // uint vestingTimeAdvisor = SECONDS_IN_DAY * 30 * 12;  // 1 year 
-   // uint vestingTimeEmployee = SECONDS_IN_DAY * 30 * 12 * 2; // 2 years 
-
-   uint cliffFounder = 3 * 60 ; // 3 phút 
-   uint cliffAdvisor =  3 * 60 ; // 3 phút 
-   uint cliffEmployee =  3 * 60 ; // 3 phút 
-   uint vestingTimeFounder =  3 * 60 ; // 3 phút 
-   uint vestingTimeAdvisor = 3 * 60 ; // 3 phút 
-   uint vestingTimeEmployee = 3 * 60 ; // 3 phút 
+   uint constant cliffFounder = 1 * 60 ; // 3 phút 
+   uint constant cliffAdvisor =  2 * 60 ; // 3 phút 
+   uint constant cliffEmployee =  3 * 60 ; // 3 phút 
+   uint constant vestingTimeFounder =  3 * 60 ; // 3 phút 
+   uint constant vestingTimeAdvisor = 3 * 60 ; // 3 phút 
+   uint constant vestingTimeEmployee = 3 * 60 ; // 3 phút 
 
    struct VestingInformation {
-      VestingWallet vestingWallet;
+      KietVestingWallet kietVestingWallet;
       Role role; // Role của người vesting
       bool isInVestingGroup;
    }
 
    mapping (address => VestingInformation) public VestingPerson;
 
-   address[] VestingGroup;
-
-   constructor(address _token) Ownable(msg.sender) {
+   constructor(address _token, address _kietVestingWalletAddress ) Ownable(msg.sender) {
       token = IERC20(_token);
+      kietVestingWalletAddress = _kietVestingWalletAddress;
    }
 
    function getBalance(address _account) external view returns (uint256){
       return token.balanceOf(_account);
    }
 
+   event VestingAdded(address indexed beneficiary, string role, uint64 vestingAmount, uint startVestingTime, uint vestingDuration);
+   event TokenClaimed(address indexed beneficiary, uint tokenClaimed );
+
    function addVesting(address _address, string memory _role) public onlyOwner returns (address) {
       require(!VestingPerson[_address].isInVestingGroup, "Person is in vesting group already");
-      
-      VestingGroup.push(_address);
 
+      address payable proxy = createClone(kietVestingWalletAddress); // clone địa chỉ của KietVestingWallet 
+      
       if (keccak256(bytes(_role)) == keccak256(bytes("Founder")))  {
-         VestingWallet _vestingWallet = new VestingWallet(_address, uint64(block.timestamp + cliffFounder), uint64(vestingTimeFounder));
-         VestingPerson[_address] = VestingInformation(_vestingWallet, Role.Founder, true);
-         token.transfer(address(_vestingWallet), 500);  
-         return address(_vestingWallet);
+         KietVestingWallet(proxy).initialized(_address, uint64(block.timestamp + cliffFounder), uint64(vestingTimeFounder)); // ép kiểu address về KietVestingWallet 
+         VestingPerson[_address] = VestingInformation(KietVestingWallet(proxy), Role.Founder, true);
+         token.transfer(address(KietVestingWallet(proxy)), 500);  
+         emit VestingAdded(_address, _role, 500, uint64(block.timestamp + cliffFounder), uint64(vestingTimeFounder));
+         return address(KietVestingWallet(proxy));
 
       } else if (keccak256(bytes(_role)) == keccak256(bytes("Advisor"))) {
-         VestingWallet _vestingWallet = new VestingWallet(_address, uint64(block.timestamp + cliffAdvisor), uint64(vestingTimeAdvisor));
-         VestingPerson[_address] = VestingInformation(_vestingWallet, Role.Advisor, true);
-         token.transfer(address(_vestingWallet), 150); 
-         return address(_vestingWallet);
+         KietVestingWallet(proxy).initialized(_address, uint64(block.timestamp + cliffAdvisor), uint64(vestingTimeAdvisor));
+         VestingPerson[_address] = VestingInformation(KietVestingWallet(proxy), Role.Advisor, true);
+         token.transfer(address(KietVestingWallet(proxy)), 150); 
+         emit VestingAdded(_address, _role, 150, uint64(block.timestamp + cliffAdvisor), uint64(vestingTimeAdvisor));
+         return address(KietVestingWallet(proxy));
 
       } else {
-         VestingWallet _vestingWallet = new VestingWallet(_address, uint64(block.timestamp + cliffEmployee), uint64(vestingTimeEmployee));
-         VestingPerson[_address] = VestingInformation(_vestingWallet, Role.Employee, true);
-         token.transfer(address(_vestingWallet), 100); 
-         return address(_vestingWallet);
+         KietVestingWallet(proxy).initialized(_address, uint64(block.timestamp + cliffEmployee), uint64(vestingTimeEmployee));
+         VestingPerson[_address] = VestingInformation(KietVestingWallet(proxy), Role.Employee, true);
+         token.transfer(address(KietVestingWallet(proxy)), 100); 
+         emit VestingAdded(_address, _role, 100, uint64(block.timestamp + cliffEmployee), uint64(vestingTimeEmployee));
+         return address(KietVestingWallet(proxy));
+
       }
    }
 
    function claimToken() public {
       require(VestingPerson[msg.sender].isInVestingGroup, "You are not in vesting group");
-      VestingPerson[msg.sender].vestingWallet.release(address(token));
+      uint tokenClaimed = getReleasableToken();
+      VestingPerson[msg.sender].kietVestingWallet.release(address(token));
+      emit TokenClaimed(msg.sender, tokenClaimed);
+   }
+
+   function getVestedToken() public view returns (uint) {
+      require(VestingPerson[msg.sender].isInVestingGroup, "You are not in vesting group");
+      return VestingPerson[msg.sender].kietVestingWallet.vestedAmount(address(token), uint64(block.timestamp));
    }
 
    function getReleasableToken() public view returns(uint256) {
       require(VestingPerson[msg.sender].isInVestingGroup, "You are not in vesting group");
-      return VestingPerson[msg.sender].vestingWallet.releasable(address(token));
+      return VestingPerson[msg.sender].kietVestingWallet.releasable(address(token));
+      
    }
 
    function getTokenAddress() public view returns(address) {
@@ -91,9 +101,11 @@ contract Vesting is Ownable {
 
 // Owner Token: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
 
-// MyToken address 30/5/2025: 0xE73E34dc58E839eF58B64B3FC81F37BC864a9065
+// KietToken address: 0xF896bB1Da84b8dDE7Ca31D79075B56e51Cdd5582
 
-// Vesting address 30/5/2025: 0x2F8895b08D8F226b19895d46154faB7096fB2593
+// KietVestingWallet address: 0xa3A518Ba4e193Fb129aa379F5916d4660f15cE5D
+
+// Vesting address: 0xDf9D0C45d97f134151a386E0AA23b09CA903c13f
 
 // Founder 1: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 
 
